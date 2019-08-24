fdd916db716ca1b89a0383f64050151a95f78b9b
tkw.tokyo <tkw.tokyo@gmail.com>
2019-08-24 10:45:22 +0900

    project作成

---
+++ ThinMP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThinMP
+//
+//  Created by tk on 2019/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
