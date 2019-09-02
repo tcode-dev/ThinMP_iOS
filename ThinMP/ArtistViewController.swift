317cb3b45a3b0910ae9b94a3dafec69464333a20
tkw.tokyo <tkw.tokyo@gmail.com>
2019-09-02 20:09:09 +0900

    アーティスト一覧を表示

--- ThinMP/ArtistViewController.swift
+++ ThinMP/ArtistViewController.swift
@@ -1,26 +1,50 @@
 import UIKit
 import MediaPlayer
 
-class ArtistViewController: UIViewController {
-
+class ArtistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+        @IBOutlet var tableView: UITableView!
+    var artists:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        artists()
+        
+       setArtistsAsync()
     }
 
-    func artists() {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artists.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "ArtistCell", for: indexPath)
+
+        cell.textLabel!.text = artists[indexPath.row]
+        
+        return cell
+    }
+
+    func setArtistsAsync() {
         MPMediaLibrary.requestAuthorization { status in
             if status == .authorized {
-                let query = MPMediaQuery.artists()
-                if let collections = query.collections {
-                    for collection in collections {
-                        if let representativeTitle = collection.representativeItem!.artist {
-                            print("アーティスト名: \(representativeTitle)  曲数: \(collection.items.count)")
-                        }
-                    }
+                self.artists = self.getArtists()
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    func getArtists() -> [String] {
+        var artists:[String] = []
+        let query = MPMediaQuery.artists()
+
+        if let collections = query.collections {
+            for collection in collections {
+                if let representativeTitle = collection.representativeItem!.artist {
+                    artists.append(representativeTitle)
+                    print("アーティスト名: \(representativeTitle)  曲数: \(collection.items.count)")
                 }
             }
         }
+
+        return artists
     }
 }
