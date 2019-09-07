e880824dcba92ed268b563091d321fea71c265ed
tkw.tokyo <tkw.tokyo@gmail.com>
2019-09-07 22:35:58 +0900

    タップした曲を再生

--- ThinMP/SongsViewController.swift
+++ ThinMP/SongsViewController.swift
@@ -6,9 +6,7 @@ class SongsViewController: UIViewController , UITableViewDelegate, UITableViewDa
     @IBOutlet var tableView: UITableView!
     
     var player: MPMusicPlayerController!
-    
-    var songTitles:[String] = []
-    var songItems:[MPMediaItem] = []
+    var songCollections:[MPMediaItemCollection] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,13 +15,13 @@ class SongsViewController: UIViewController , UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return songTitles.count;
+        return songCollections.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath)
         
-        cell.textLabel!.text = songTitles[indexPath.row]
+        cell.textLabel!.text = songCollections[indexPath.row].representativeItem!.title
         
         return cell
     }
@@ -31,7 +29,8 @@ class SongsViewController: UIViewController , UITableViewDelegate, UITableViewDa
     func setSongsAsync() {
         MPMediaLibrary.requestAuthorization { status in
             if status == .authorized {
-                self.songTitles = self.getSongs()
+                self.songCollections = MPMediaQuery.songs().collections!
+                
                 DispatchQueue.global(qos: .userInitiated).async {
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
@@ -41,32 +40,11 @@ class SongsViewController: UIViewController , UITableViewDelegate, UITableViewDa
         }
     }
     
-    func getSongs() -> [String] {
-        var songs:[String] = []
-        let query = MPMediaQuery.songs()
-        
-        if let collections = query.collections {
-            for collection in collections {
-                if collection.representativeItem != nil, let representativeTitle = collection.representativeItem!.title {
-                    self.songItems.append(collection.representativeItem!)
-                    songs.append(representativeTitle)
-                }
-            }
-        }
-        
-        return songs
-    }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //        let item = songItems[indexPath.row]
-        
         player = MPMusicPlayerController.applicationMusicPlayer
-        //        player = MPMusicPlayerController.systemMusicPlayer
         player.repeatMode = .none
         
-        let descriptor = MPMusicPlayerMediaItemQueueDescriptor.init(query: MPMediaQuery.songs())
-        //        let list = MPMediaQuery.songs().collections
-        //        let descriptor = MPMusicPlayerMediaItemQueueDescriptor.init(MPMediaItemCollection: list)
+        let descriptor = MPMusicPlayerMediaItemQueueDescriptor.init(itemCollection: songCollections[indexPath.row])
         
         player.setQueue(with: descriptor)
         player.play()
