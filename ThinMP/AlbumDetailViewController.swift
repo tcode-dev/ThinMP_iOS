3716c7e2e9c81925f1505e36bdbff378f03bf1ba
tkw.tokyo <tkw.tokyo@gmail.com>
2019-09-16 22:28:39 +0900

    アーティスト詳細 画像を表示

--- ThinMP/AlbumDetailViewController.swift
+++ ThinMP/AlbumDetailViewController.swift
@@ -6,8 +6,9 @@ class AlbumDetailViewController: UIViewController {
     var arg: String!
     
     @IBOutlet var primaryText: UILabel!
-    
     @IBOutlet var secondlyText: UILabel!
+    @IBOutlet var artworkView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,15 +19,20 @@ class AlbumDetailViewController: UIViewController {
         let property = MPMediaPropertyPredicate(value: self.arg, forProperty: MPMediaItemPropertyAlbumPersistentID)
         let query = MPMediaQuery.albums()
         query.addFilterPredicate(property)
-        let albums = query.items as! [MPMediaItem]
-
-        if albums.count > 0 {
-            let album = albums[0]
+        let albums = query.items
+        
+        if (albums?.count)! > 0 {
+            let album = albums?[0]
             DispatchQueue.global(qos: .userInitiated).async {
                 DispatchQueue.main.async {
-                    self.primaryText.text = album.albumTitle
-                    self.secondlyText.text = album.albumArtist
-                    album.artwork
+                    self.primaryText.text = album!.albumTitle
+                    self.secondlyText.text = album!.albumArtist
+                    
+                    if let artwork = album!.artwork {
+                        self.artworkView.contentMode = UIView.ContentMode.scaleAspectFill
+                        self.artworkView.clipsToBounds = true
+                        self.artworkView.image = artwork.image(at: self.artworkView.bounds.size)
+                    }
                 }
             }
         }
