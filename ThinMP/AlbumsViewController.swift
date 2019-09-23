07069b2d7c7426a96a855bbd3c9bbe17316a313c
tkw.tokyo <tkw.tokyo@gmail.com>
2019-09-24 08:54:09 +0900

    アルバム一覧 画像追加。横並び。

--- ThinMP/AlbumsViewController.swift
+++ ThinMP/AlbumsViewController.swift
@@ -1,25 +1,47 @@
 import UIKit
 import MediaPlayer
 
-class AlbumsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-
-    @IBOutlet var tableView: UITableView!
+class AlbumsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    @IBOutlet var albumCollectionView: UICollectionView!
+    
     var albumCollections:[MPMediaItemCollection] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        albumCollectionView.collectionViewLayout = layout
+
         setAlbumsAsync()
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return albumCollections.count;
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        let imageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let primaryLabel = cell.contentView.viewWithTag(2) as! UILabel
+        let secondaryLabel = cell.contentView.viewWithTag(3) as! UILabel
         
-        cell.textLabel!.text = albumCollections[indexPath.row].representativeItem!.albumTitle
+        if let item = albumCollections[indexPath.row].representativeItem {
+            if let albumTitle = item.albumTitle {
+                primaryLabel.text = albumTitle
+            }
+            
+            if let artist = item.artist {
+                secondaryLabel.text = artist
+            }
+            
+            imageView.image = nil
+            if let artwork = item.artwork {
+                imageView.contentMode = UIView.ContentMode.scaleAspectFill
+                imageView.clipsToBounds = true
+                imageView.image = artwork.image(at: imageView.bounds.size)
+            }
+        }
         
         return cell
     }
@@ -30,20 +52,25 @@ class AlbumsViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 self.albumCollections = MPMediaQuery.albums().collections!
                 DispatchQueue.global(qos: .userInitiated).async {
                     DispatchQueue.main.async {
-                        self.tableView.reloadData()
+                        self.albumCollectionView.reloadData()
                     }
                 }
             }
         }
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let storyboard: UIStoryboard = self.storyboard!
-        
         let nextView = storyboard.instantiateViewController(withIdentifier: "AlbumDetail") as! AlbumDetailViewController
         
         nextView.arg = String(albumCollections[indexPath.row].persistentID)
-
+        
         self.present(nextView, animated: true, completion: nil)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let horizontalSpace : CGFloat = 20
+        let cellSize : CGFloat = self.view.bounds.width / 2 - horizontalSpace
+        return CGSize(width: cellSize, height: cellSize)
     }
 }
