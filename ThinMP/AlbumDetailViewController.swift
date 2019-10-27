b1f1c986cea8a741b9bcf1da21816ba45f06f8c9
tkw.tokyo <tkw.tokyo@gmail.com>
2019-10-27 19:42:19 +0900

    アルバム詳細 table header

--- ThinMP/AlbumDetailViewController.swift
+++ ThinMP/AlbumDetailViewController.swift
@@ -1,59 +1,68 @@
 import UIKit
 import MediaPlayer
 
-class AlbumDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AlbumDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
     var identifier: MPMediaEntityPersistentID!
     var songCollections:[MPMediaItemCollection] = []
+//    var headerViewWidth: CGFloat!
+//    var headerViewHeight: CGFloat!
     
+    @IBOutlet var headerView: UIView!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var primaryText: UILabel!
-    @IBOutlet var secondlyText: UILabel!
-    @IBOutlet var artworkView: UIImageView!
+    @IBOutlet var secondaryText: UILabel!
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UINib(nibName: "TableViewTrackCell", bundle: nil),forCellReuseIdentifier:"customTableViewTrackCell")
         
-        setUpWithPermissionCheck()
+        setupWithPermissionCheck()
     }
     
-    func setUpWithPermissionCheck() {
+    func setupWithPermissionCheck() {
         if MPMediaLibrary.authorizationStatus() == .authorized {
-            setUp()
+            setup()
         } else {
             MPMediaLibrary.requestAuthorization { status in
                 if status == .authorized {
-                    self.setUp()
+                    self.setup()
                 }
             }
         }
     }
     
-    func setUp() {
+    
+    func setup() {
         let property = MPMediaPropertyPredicate(value: self.identifier, forProperty: MPMediaItemPropertyAlbumPersistentID)
         let query = MPMediaQuery.songs()
         query.addFilterPredicate(property)
         let albums = query.items!.filter({$0.albumTitle != nil})
         songCollections = query.collections!
-
+        
         if (albums.count) > 0 {
             let album = albums[0]
             DispatchQueue.global(qos: .userInitiated).async {
                 DispatchQueue.main.async {
-                    self.primaryText.text = album.albumTitle
-                    self.secondlyText.text = album.albumArtist
+                    
                     
                     if let artwork = album.artwork {
-                        self.artworkView.contentMode = UIView.ContentMode.scaleAspectFill
-                        self.artworkView.clipsToBounds = true
-                        self.artworkView.image = artwork.image(at: self.artworkView.bounds.size)
+                        self.imageView.contentMode = UIView.ContentMode.scaleAspectFill
+                        self.imageView.clipsToBounds = true
+                        self.imageView.image = artwork.image(at: self.imageView.bounds.size)
                     }
+                    self.primaryText.text = album.albumTitle
+                    self.secondaryText.text = album.albumArtist
                     
                     self.tableView.reloadData()
                 }
             }
         }
+        
+//        headerViewWidth = headerView.bounds.width
+//        headerViewHeight = headerView.bounds.height
+//        headerView.backgroundColor = UIColor.clear
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -86,4 +95,11 @@ class AlbumDetailViewController: UIViewController, UITableViewDelegate, UITableV
         let musicService = MusicService.sharedInstance()
         musicService.start(itemCollection: songCollections[indexPath.row])
     }
+    
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let scroll = scrollView.contentOffset.y
+//        if (scroll > 0 && scroll < headerViewHeight) {
+//            pageView.frame = CGRect.init(x: 0, y: -scroll, width: headerViewWidth, height: headerViewHeight)
+//        }
+//    }
 }
