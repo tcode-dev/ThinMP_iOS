f10ed330a42b5d601a08a6f0271dde1da8848c77
tkw.tokyo <tkw.tokyo@gmail.com>
2019-10-28 23:08:33 +0900

    アーティスト詳細 アルバム一覧

--- ThinMP/View/ArtistDetailViewController.swift
+++ ThinMP/View/ArtistDetailViewController.swift
@@ -3,58 +3,121 @@ import MediaPlayer
 
 class ArtistDetailViewController: UIViewController {
     var identifier: MPMediaEntityPersistentID!
+    var albums: [Album] = []
     
     @IBOutlet var artworkView: UIImageView!
     @IBOutlet var primaryText: UILabel!
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpWithPermissionCheck()
+        collectionView.register(UINib(nibName: "CollectionViewAlubumCell", bundle: nil), forCellWithReuseIdentifier: "customCollectionViewAlubumCell")
+        collectionView.register(UINib(nibName: "TableViewTrackCell", bundle: nil),forCellWithReuseIdentifier:"customTableViewTrackCell")
+        
+        setupWithPermissionCheck()
     }
     
-    func setUpWithPermissionCheck() {
+    func setupWithPermissionCheck() {
         if MPMediaLibrary.authorizationStatus() == .authorized {
-            setUp()
+            setup()
         } else {
             MPMediaLibrary.requestAuthorization { status in
                 if status == .authorized {
-                    self.setUp()
+                    self.setup()
                 }
             }
         }
     }
     
-    func setUp() {
+    func setup() {
         let property = MPMediaPropertyPredicate(value: self.identifier, forProperty: MPMediaItemPropertyArtistPersistentID)
         let query = MPMediaQuery.artists()
         query.addFilterPredicate(property)
         let songs = query.items!
-        let albums = Dictionary.init(grouping: songs) { song -> MPMediaEntityPersistentID in
+        let albumMap = Dictionary.init(grouping: songs) { song -> MPMediaEntityPersistentID in
             return song.albumPersistentID
         }
-//        albums.forEach { (album) in
-//            let (albumPersistentID, mpMediaItems) = album
-//            mpMediaItems.forEach({ (mpMediaItem) in
-//                NSLog(mpMediaItem.title!)
-//            })
-//        }
+        
+        albums = albumMap.map { (arg0) -> Album in
+            let (persistentID, songs) = arg0
+            let title = songs.first(where: { (song) -> Bool in
+                (song.albumTitle != nil)
+            })?.title
+            let artist = songs.first(where: { (song) -> Bool in
+                (song.albumArtist != nil)
+            })?.artist
+            let artwork = songs.first(where: { (song) -> Bool in
+                (song.artwork != nil)
+            })?.artwork
+            
+            return Album(persistentID: persistentID, title: title, artist: artist, artwork: artwork, songs: songs)
+        }
+        
+        albums.sort(by: {$0.title! < $1.title! })
         
         DispatchQueue.global(qos: .userInitiated).async {
             DispatchQueue.main.async {
-                self.primaryText.text = songs.first(where: { (song) -> Bool in
-                    (song.artist != nil)
+                self.primaryText.text = self.albums.first(where: { (album) -> Bool in
+                    (album.artist != nil)
                 })?.artist
-
-                if let artwork = songs.first(where: { (song) -> Bool in
-                    (song.artwork != nil)
+                
+                if let artwork = self.albums.first(where: { (album) -> Bool in
+                    (album.artwork != nil)
                 })?.artwork {
                     self.artworkView.contentMode = UIView.ContentMode.scaleAspectFill
                     self.artworkView.clipsToBounds = true
                     self.artworkView.image = artwork.image(at: self.artworkView.bounds.size)
                 }
-
-                //                    self.tableView.reloadData()
             }
         }
+    }
+}
+
+extension ArtistDetailViewController : UICollectionViewDataSource,
+UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return albums.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCollectionViewAlubumCell", for: indexPath) as! CollectionViewAlubumCell
+        
+        let album = albums[indexPath.row]
+        if let title = album.title {
+            cell.primaryText.text = title
+        }
+        
+        if let artist = album.artist {
+            cell.secondaryText.text = artist
+        }
+        
+        cell.artworkView.image = nil
+        if let artwork = album.artwork {
+            cell.artworkView.contentMode = UIView.ContentMode.scaleAspectFill
+            cell.artworkView.clipsToBounds = true
+            cell.artworkView.image = artwork.image(at: cell.artworkView!.bounds.size)
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard: UIStoryboard = self.storyboard!
+        let nextView = storyboard.instantiateViewController(withIdentifier: "AlbumDetail") as! AlbumDetailViewController
+        
+        nextView.identifier = albums[indexPath.row].persistentID
+        
+        self.present(nextView, animated: true, completion: nil)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let horizontalSpace : CGFloat = 10
+        let width : CGFloat = self.view.bounds.width / 2 - horizontalSpace
+        let vertical : CGFloat = 42
+        let height : CGFloat = width + vertical
+        
+        return CGSize(width: width, height: height)
     }
 }
