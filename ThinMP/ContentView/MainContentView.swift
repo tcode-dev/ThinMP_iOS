87b3f0945a41dbc02892602d556d62471563ff1e
tkw.tokyo <tkw.tokyo@gmail.com>
2020-01-05 13:15:51 +0900

    SwiftUIに移行
    SwiftUIの新規プロジェクトを作成して差分を反映した

---
+++ ThinMP/ContentView/MainContentView.swift
@@ -0,0 +1,22 @@
+//
+//  MainContentView.swift
+//  ThinMP
+//
+//  Created by tk on 2020/01/04.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        List {
+            Text("Artists")
+        }
+    }
+}
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
