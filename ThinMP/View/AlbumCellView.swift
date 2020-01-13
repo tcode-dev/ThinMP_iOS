22d62eb764dc257faf132c952553cda64816cfb3
tkw.tokyo <tkw.tokyo@gmail.com>
2020-01-13 15:38:54 +0900

    アルバム一覧　アーティスト名追加。スタイル調整。

--- ThinMP/View/AlbumCellView.swift
+++ ThinMP/View/AlbumCellView.swift
@@ -13,13 +13,15 @@ struct AlbumCellView: View {
     var cgSize: CGSize
     
     var body: some View {
-        VStack{
+        VStack(){
             Image(uiImage: self.album.artwork?.image(at: cgSize) ?? UIImage())
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(1, contentMode: .fit)
             
             Text(self.album.title ?? "unknown").foregroundColor(.primary).lineLimit(1)
+
+            Text(self.album.artist ?? "unknown").foregroundColor(.secondary).lineLimit(1)
         }.frame(width: width)
     }
 }
--- ThinMP/View/AlbumsView.swift
+++ ThinMP/View/AlbumsView.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 struct AlbumsView: View {
     let colCount = 2
+    let space: CGFloat = 20
     var list: [[Album]]
     var width: CGFloat
     var cgSize: CGSize
     
     init(list: [Album], width: CGFloat) {
         self.list = list.chunked(into: colCount)
-        self.width = width / CGFloat(colCount)
+        self.width = (width - (space * CGFloat(colCount + 1))) / CGFloat(colCount)
         self.cgSize = CGSize(width: self.width, height: self.width)
     }
     var body: some View {
         ForEach(list.indices) { row in
-            HStack {
+            HStack(spacing: self.space) {
                 ForEach(self.list[row].indices) { col in
                     NavigationLink(destination: AlbumDetailContentView(album: self.list[row][col])) {
                         AlbumCellView(album: self.list[row][col], width: self.width, cgSize: self.cgSize)
