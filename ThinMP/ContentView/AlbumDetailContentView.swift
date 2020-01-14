0281226ddd6966626b6c929dc6904a2bde896475
tkw.tokyo <tkw.tokyo@gmail.com>
2020-01-14 09:38:53 +0900

    アルバム詳細 アルバム名、アーティスト名、透過グラデーションを追加

--- ThinMP/ContentView/AlbumDetailContentView.swift
+++ ThinMP/ContentView/AlbumDetailContentView.swift
@@ -16,10 +16,17 @@ struct AlbumDetailContentView: View {
         GeometryReader { geometry in
             ScrollView {
                 VStack(alignment: .leading) {
-                    Image(uiImage: self.albumDetail.artwork?.image(at: CGSize(width: geometry.size.width, height: geometry.size.width)) ?? UIImage())
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
+                    ZStack(alignment: .bottom) {
+                        Image(uiImage: self.albumDetail.artwork?.image(at: CGSize(width: geometry.size.width, height: geometry.size.width)) ?? UIImage())
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                        LinearGradient(gradient: Gradient(colors: [Color.init(Color.RGBColorSpace.sRGB, red: 1, green: 1, blue: 1, opacity: 0), .white]), startPoint: .top, endPoint: .bottom).frame(height: 200)
+                        VStack {
+                            PrimaryTextView(self.albumDetail.title)
+                            SecondaryTextView(self.albumDetail.artist)
+                        }
+                    }
                     
                     ForEach(self.albumDetail.songs){ song in
                         SongRowView(song: song)
