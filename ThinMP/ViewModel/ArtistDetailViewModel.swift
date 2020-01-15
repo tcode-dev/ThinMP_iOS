5a20c692f3a6a365b3d37e97f47fe5e146882784
tkw.tokyo <tkw.tokyo@gmail.com>
2020-01-16 08:34:32 +0900

    アーティスト詳細 アーティスト、アルバムからデータ取得

--- ThinMP/ViewModel/ArtistDetailViewModel.swift
+++ ThinMP/ViewModel/ArtistDetailViewModel.swift
@@ -13,54 +13,80 @@ class ArtistDetailViewModel: ObservableObject {
     @Published var artwork: MPMediaItemArtwork?
     @Published var albums: [Album] = []
     @Published var songs: [Song] = []
-    @Published var mediaItems: [MPMediaItem] = []
+    @Published var songCollections: [MPMediaItemCollection] = []
     @Published var albumCount: Int = 0
     @Published var songCount: Int = 0
     
     init(persistentId: MPMediaEntityPersistentID) {
         self.persistentId = persistentId
-        
-        self.set()
+        if MPMediaLibrary.authorizationStatus() == .authorized {
+            fetch()
+        } else {
+            MPMediaLibrary.requestAuthorization { status in
+                if status == .authorized {
+                    self.fetch()
+                }
+            }
+        }
     }
     
-    func set() {
+    func fetch() {
+        if let artist = getArtist() {
+            self.name = artist.name
+        }
+        
+        let albums = getAlbums()
+
+        if !albums.isEmpty {
+            self.albums = albums
+            self.albumCount = albums.count
+            self.artwork = self.albums.first(where: { (album) -> Bool in
+                (album.artwork != nil)
+            })?.artwork
+        }
+
+        let songs = getSongs()
+
+        if !songs.songs.isEmpty && !songs.songCollections.isEmpty {
+            self.songs = songs.songs
+            self.songCount = songs.songs.count
+            self.songCollections = songs.songCollections
+        }
+    }
+    
+    func getArtist() -> Artist? {
         let property = MPMediaPropertyPredicate(value: self.persistentId, forProperty: MPMediaItemPropertyArtistPersistentID)
         let query = MPMediaQuery.artists()
+        
         query.addFilterPredicate(property)
-        self.mediaItems = query.items!
-        let albumMap = Dictionary.init(grouping: self.mediaItems) { song -> MPMediaEntityPersistentID in
-            return song.albumPersistentID
+        
+        return query.collections!.map{
+            return Artist(persistentId: $0.representativeItem?.artistPersistentID, name: $0.representativeItem?.artist)
+        }.first
+    }
+
+    func getAlbums() -> [Album] {
+        let property = MPMediaPropertyPredicate(value: self.persistentId, forProperty: MPMediaItemPropertyArtistPersistentID)
+        let query = MPMediaQuery.albums()
+        
+        query.addFilterPredicate(property)
+        
+        return query.collections!.map{
+            return Album(persistentID: $0.representativeItem?.albumPersistentID, title: $0.representativeItem?.albumTitle, artist: $0.representativeItem?.artist, artwork: $0.representativeItem?.artwork)
+        }
+    }
+    
+    func getSongs() -> (songs: [Song], songCollections: [MPMediaItemCollection]) {
+        let property = MPMediaPropertyPredicate(value: self.persistentId, forProperty: MPMediaItemPropertyArtistPersistentID)
+        let query = MPMediaQuery.songs()
+        
+        query.addFilterPredicate(property)
+        
+        let songCollections = query.collections!
+        let songs = songCollections.map{
+            return Song(title: $0.representativeItem?.title, artist: $0.representativeItem?.artist, artwork: $0.representativeItem?.artwork)
         }
         
-        self.albums = albumMap.map {
-            let (persistentID, songs) = $0
-            let title = songs.first(where: { (song) -> Bool in
-                (song.albumTitle != nil)
-            })?.title
-            let artist = songs.first(where: { (song) -> Bool in
-                (song.albumArtist != nil)
-            })?.artist
-            let artwork = songs.first(where: { (song) -> Bool in
-                (song.artwork != nil)
-            })?.artwork
-            
-            return Album(persistentID: persistentID, title: title, artist: artist, artwork: artwork)
-        }
-        self.albumCount = self.albums.count
-        
-        self.songs = mediaItems.map {
-            return Song(title: $0.title, artist: $0.artist, artwork: $0.artwork)
-        }
-        self.songCount = self.songs.count
-        
-        self.albums.sort(by: {$0.title! < $1.title! })
-        
-        self.name = self.albums.first(where: { (album) -> Bool in
-            (album.artist != nil)
-        })?.artist
-        
-        self.artwork = self.albums.first(where: { (album) -> Bool in
-            (album.artwork != nil)
-        })?.artwork
+        return (songs, songCollections);
     }
 }
