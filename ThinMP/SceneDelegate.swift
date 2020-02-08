b785d98cb8fe4f1febb9852a3c56258326574007
tkw.tokyo <tkw.tokyo@gmail.com>
2020-02-08 12:39:21 +0900

    アルバム一覧 ListのborderとselectionStyleを無効

--- ThinMP/SceneDelegate.swift
+++ ThinMP/SceneDelegate.swift
@@ -18,6 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
+        // Listのborderを無効
+        UITableView.appearance().separatorStyle = .none
+        // ListのselectionStyleを無効
+        UITableViewCell.appearance().selectionStyle = .none
+
         let musicPlayer = MusicPlayer()
 
         // Create the SwiftUI view that provides the window contents.
