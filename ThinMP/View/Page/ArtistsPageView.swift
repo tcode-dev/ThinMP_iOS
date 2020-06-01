1ef519a0e6464dddd0ff4fa8148e01bffd9b801a
tkw.tokyo <tkw.tokyo@gmail.com>
2020-06-02 08:10:45 +0900

    アーティスト一覧 表示に時間が掛かるのでScrollViewをListに変更した

--- ThinMP/View/Page/ArtistsPageView.swift
+++ ThinMP/View/Page/ArtistsPageView.swift
@@ -13,14 +13,9 @@ struct ArtistsPageView: View {
     var body: some View {
         ZStack(alignment: .top) {
             ListHeaderView(primaryText: "artists")
-            ScrollView{
-                VStack(alignment: .leading) {
-                    ForEach(artists.list) { artist in
-                        NavigationLink(destination: ArtistDetailPageView(artist: artist)) {
-                            ArtistRowView(artist: artist)
-                        }
-                        Divider()
-                    }
+            List(self.artists.list.indices) { index in
+                NavigationLink(destination: ArtistDetailPageView(artist: self.artists.list[index])) {
+                    ArtistRowView(artist: self.artists.list[index])
                 }
             }
             .padding(.init(top: 50, leading: 0, bottom: 0, trailing: 0))
