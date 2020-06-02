b291e8792d629fc7daf1607ec672efc570b40a78
tkw.tokyo <tkw.tokyo@gmail.com>
2020-06-03 07:39:50 +0900

    メニュー 開閉

--- ThinMP/View/Button/MenuButtonView.swift
+++ ThinMP/View/Button/MenuButtonView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct MenuButtonView: View {
+    @State var isOpen: Bool = false
+
     var body: some View {
         Button(action: {
+            self.isOpen.toggle()
         }) {
             Image("MenuButton")
                 .renderingMode(.original)
+                .frame(width: 50, height: 50)
+        }
+        .actionSheet(isPresented: $isOpen) {
+            ActionSheet(title: Text("What action?"),
+                        message: Text("Pick one"),
+                        buttons: [ .default(Text("Option 1"))
+                            , .default(Text("Option 2"))
+                            , .cancel()
+            ])
         }
         .frame(width: 50, height: 50)
     }
