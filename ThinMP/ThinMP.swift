0b3dc07af4810dd0ee1e4fa80c4a60fc5f3d8f84
tkw.tokyo <tkw.tokyo@gmail.com>
2020-12-23 08:33:55 +0900

    AppDelegate、SceneDelegateを削除

---
+++ ThinMP/ThinMP.swift
@@ -0,0 +1,17 @@
+//
+//  ThinMP.swift
+//  ThinMP
+//
+//  Created by tk on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ThinMP: App {
+    var body: some Scene {
+        WindowGroup {
+            MainPageView()
+        }
+    }
+}
