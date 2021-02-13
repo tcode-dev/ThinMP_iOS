742067b27d34b1a68c7e49a5fe7e7223be189659
tkwtokyo <tkw.tokyo@gmail.com>
2021-02-13 18:48:22 +0900

    リファクタ ArtistSongListView

--- ThinMP/View/List/ArtistSongListView.swift
+++ ThinMP/View/List/ArtistSongListView.swift
@@ -1,5 +1,5 @@
 //
-//  ArtistSongsView.swift
+//  ArtistSongListView.swift
 //  ThinMP
 //
 //  Created by tk on 2020/01/14.
@@ -9,12 +9,11 @@ import SwiftUI
 
 struct ArtistSongListView: View {
     var list: [Song]
+
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(list) { song in
-                ArtistSongRowView(song: song).padding(0)
-                Divider()
-            }
+        ForEach(list) { song in
+            ArtistSongRowView(song: song).padding(0)
+            Divider()
         }
     }
 }
