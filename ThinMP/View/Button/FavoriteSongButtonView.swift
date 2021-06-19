768618afdb69708f16262e22e8fa8895ef1ec91c
tkwtokyo <tkw.tokyo@gmail.com>
2021-06-19 17:51:57 +0900

    お気に入りの曲登録・削除後ボタンが切り替わらないのを修正

--- ThinMP/View/Button/FavoriteSongButtonView.swift
+++ ThinMP/View/Button/FavoriteSongButtonView.swift
@@ -8,28 +8,64 @@
 import SwiftUI
 import MediaPlayer
 
+// 登録、削除後にボタンが切り替わらないので初回描画と2回目以降の描画で処理を分ける
+// スクロールで画面表出した時にinitが呼ばれるのでinitでは処理しない
+// 長押しで表出した時にbodyが呼ばれるのでbodyで処理する
 struct FavoriteSongButtonView: View {
     private let ADD_TEXT: String = "お気に入りに追加"
     private let DELETE_TEXT: String = "お気に入りから削除"
 
+    @State private var displayed: Bool = false
+    @State private var exists: Bool = false
+
     let persistentId: MPMediaEntityPersistentID
 
     var body: some View {
-        let favoriteSongRegister = FavoriteSongRegister()
+        if (!displayed) {
+            // 初回描画時
+            let register = FavoriteSongRegister()
 
-        if (!favoriteSongRegister.exists(persistentId: persistentId)) {
-            // add
-            return Button(action: {
-                favoriteSongRegister.add(persistentId: persistentId)
-            }) {
-                Text(ADD_TEXT)
+            if (!register.exists(persistentId: persistentId)) {
+                return Button(action: {
+                    let register = FavoriteSongRegister()
+
+                    register.add(persistentId: persistentId)
+                    exists = true
+                    displayed.toggle()
+                }) {
+                    Text(ADD_TEXT)
+                }
+            } else {
+                return Button(action: {
+                    let register = FavoriteSongRegister()
+
+                    register.delete(persistentId: persistentId)
+                    exists = false
+                    displayed.toggle()
+                }) {
+                    Text(DELETE_TEXT)
+                }
             }
         } else {
-            // delete
-            return Button(action: {
-                favoriteSongRegister.delete(persistentId: persistentId)
-            }) {
-                Text(DELETE_TEXT)
+            // 2回目以降
+            if (!exists) {
+                return Button(action: {
+                    let register = FavoriteSongRegister()
+
+                    register.add(persistentId: persistentId)
+                    exists.toggle()
+                }) {
+                    Text(ADD_TEXT)
+                }
+            } else {
+                return Button(action: {
+                    let register = FavoriteSongRegister()
+
+                    register.delete(persistentId: persistentId)
+                    exists.toggle()
+                }) {
+                    Text(DELETE_TEXT)
+                }
             }
         }
     }
