b3f58d2f425c7d136e4d2886b76d200ad768301d
tkwtokyo <tkw.tokyo@gmail.com>
2021-06-19 20:32:22 +0900

    ショートカットの登録・削除後ボタンが切り替わらないのを修正

--- ThinMP/View/Button/ShortcutButtonView.swift
+++ ThinMP/View/Button/ShortcutButtonView.swift
@@ -12,25 +12,56 @@ struct ShortcutButtonView: View {
     private let ADD_TEXT: String = "ショートカットに追加"
     private let DELETE_TEXT: String = "ショートカットから削除"
 
+    @State private var displayed: Bool = false
+    @State private var exists: Bool = false
+
     let itemId: ShortcutItemIdProtocol
     let type: ShortcutType
 
     var body: some View {
-        let register = ShortcutRegister()
+        if (!displayed) {
+            let register = ShortcutRegister()
 
-        if (!register.exists(itemId: itemId, type: type)) {
-            // add
-            return Button(action: {
-                register.add(itemId: itemId, type: type)
-            }) {
-                Text(ADD_TEXT)
+            if (!register.exists(itemId: itemId, type: type)) {
+                return Button(action: {
+                    let register = ShortcutRegister()
+
+                    register.add(itemId: itemId, type: type)
+                    exists = true
+                    displayed.toggle()
+                }) {
+                    Text(ADD_TEXT)
+                }
+            } else {
+                return Button(action: {
+                    let register = ShortcutRegister()
+
+                    register.delete(itemId: itemId, type: type)
+                    exists = false
+                    displayed.toggle()
+                }) {
+                    Text(DELETE_TEXT)
+                }
             }
         } else {
-            // delete
-            return Button(action: {
-                register.delete(itemId: itemId, type: type)
-            }) {
-                Text(DELETE_TEXT)
+            if (!exists) {
+                return Button(action: {
+                    let register = ShortcutRegister()
+
+                    register.add(itemId: itemId, type: type)
+                    exists.toggle()
+                }) {
+                    Text(ADD_TEXT)
+                }
+            } else {
+                return Button(action: {
+                    let register = ShortcutRegister()
+
+                    register.delete(itemId: itemId, type: type)
+                    exists.toggle()
+                }) {
+                    Text(DELETE_TEXT)
+                }
             }
         }
     }
