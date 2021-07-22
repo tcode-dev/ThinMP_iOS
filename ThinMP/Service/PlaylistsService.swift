76854871259e5c597cffcd5bf000218c9496eae4
tkwtokyo <tkw.tokyo@gmail.com>
2021-07-22 16:36:45 +0900

    Playlists validation

--- ThinMP/Service/PlaylistsService.swift
+++ ThinMP/Service/PlaylistsService.swift
@@ -13,21 +13,10 @@ struct PlaylistsService {
         let playlists = playlistRepository.findAll()
 
         return playlists.map { playlist in
-            createModel(playlist: playlist)
+            let playlistDetailService = PlaylistDetailService()
+            let playlistDetailModel = playlistDetailService.findById(playlistId: PlaylistId(id: playlist.id))
+
+            return PlaylistModel(playlistId: PlaylistId(id: playlist.id), primaryText: playlist.name, artwork: playlistDetailModel.artwork)
         }
-    }
-
-    private func createModel(playlist: PlaylistRealmModel) -> PlaylistModel {
-        let playlistSongs = playlist.songs.sorted(byKeyPath: PlaylistSongRealmModel.ORDER)
-        let songIds = Array(playlistSongs.map { SongId(id: UInt64($0.songId)!) })
-        let repository = SongRepository()
-        let songs = repository.findByIds(songIds: songIds)
-        let sorted = songIds.map { songId in songs.first { songId.equals($0.songId) }! }
-        let arrayed = Array(sorted)
-        let artwork = arrayed.first(where: { song -> Bool in
-            song.artwork != nil
-        })?.artwork
-
-        return PlaylistModel(playlistId: PlaylistId(id: playlist.id), primaryText: playlist.name, artwork: artwork)
     }
 }
