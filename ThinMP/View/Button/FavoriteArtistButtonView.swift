970716cde7ddd475ad6777d464948dd7be9e64d3
tkwtokyo <tkw.tokyo@gmail.com>
2021-07-23 16:02:15 +0900

    refactor FavoriteArtistButtonView

--- ThinMP/View/Button/FavoriteArtistButtonView.swift
+++ ThinMP/View/Button/FavoriteArtistButtonView.swift
@@ -9,7 +9,7 @@ import MediaPlayer
 import SwiftUI
 
 struct FavoriteArtistButtonView: View {
-    @State private var displayed: Bool = false
+    @State private var initialDisplay: Bool = true
     @State private var exists: Bool = false
 
     private let artistId: ArtistId
@@ -21,54 +21,48 @@ struct FavoriteArtistButtonView: View {
     }
 
     var body: some View {
-        if !displayed {
+        Group {
+            if initialDisplay {
+                let register = FavoriteArtistRegister()
+
+                if !register.exists(artistId: artistId) {
+                    createAddButton()
+                } else {
+                    createRemoveButton()
+                }
+            } else {
+                if !exists {
+                    createAddButton()
+                } else {
+                    createRemoveButton()
+                }
+            }
+        }
+    }
+
+    private func createAddButton() -> some View {
+        return Button(action: {
             let register = FavoriteArtistRegister()
 
-            if !register.exists(artistId: artistId) {
-                return Button(action: {
-                    let register = FavoriteArtistRegister()
+            register.add(artistId: artistId)
+            exists = true
+            initialDisplay = false
+            callback()
+        }) {
+            Text("AddFavorites")
+        }
+    }
 
-                    register.add(artistId: artistId)
-                    exists = true
-                    displayed.toggle()
-                    callback()
-                }) {
-                    Text("AddFavorites")
-                }
-            } else {
-                return Button(action: {
-                    let register = FavoriteArtistRegister()
+    private func createRemoveButton() -> some View {
+        Button(action: {
+            let register = FavoriteArtistRegister()
 
-                    register.delete(artistId: artistId)
-                    exists = false
-                    displayed.toggle()
-                    callback()
-                }) {
-                    Text("RemoveFavorites")
-                }
-            }
-        } else {
-            if !exists {
-                return Button(action: {
-                    let register = FavoriteArtistRegister()
-
-                    register.add(artistId: artistId)
-                    exists.toggle()
-                    callback()
-                }) {
-                    Text("AddFavorites")
-                }
-            } else {
-                return Button(action: {
-                    let register = FavoriteArtistRegister()
-
-                    register.delete(artistId: artistId)
-                    exists.toggle()
-                    callback()
-                }) {
-                    Text("RemoveFavorites")
-                }
-            }
+            register.delete(artistId: artistId)
+            exists = false
+            initialDisplay = false
+            callback()
+        }) {
+            Text("RemoveFavorites")
         }
     }
 }
