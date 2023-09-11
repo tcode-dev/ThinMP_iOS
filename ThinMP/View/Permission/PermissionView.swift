1f19198061ef399ddcc75d5e9028094dcde4d09c
tcode <tk@tcode.dev>
2023-09-11 18:52:36 +0900

    create PermissionView

---
+++ ThinMP/View/Permission/PermissionView.swift
@@ -0,0 +1,14 @@
+//
+//  PermissionView.swift
+//  ThinMP
+//
+//  Created by tk on 2023/09/11.
+//
+
+import SwiftUI
+
+struct PermissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
