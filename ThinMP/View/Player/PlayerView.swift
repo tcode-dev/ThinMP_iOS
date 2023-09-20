d2c3690c65d46a1ca220d1689bf9dd04598142c3
tcode <tk@tcode.dev>
2023-09-20 20:16:33 +0900

    design player for iPad, iPhone

--- ThinMP/View/Player/PlayerView.swift
+++ ThinMP/View/Player/PlayerView.swift
@@ -14,6 +14,7 @@ struct PlayerView: View {
     @State var seeking: Bool = false
     @State private var showingPopup: Bool = false
     private let callback: () -> Void
+    private let isPad = UIDevice.current.userInterfaceIdiom == .pad
 
     init(callback: @escaping () -> Void = {}) {
         self.callback = callback
@@ -22,6 +23,7 @@ struct PlayerView: View {
     var body: some View {
         GeometryReader { geometry in
             let size = geometry.size.width
+            let height = geometry.size.height
 
             ZStack(alignment: .top) {
                 ZStack {
@@ -34,148 +36,131 @@ struct PlayerView: View {
                 }
                 .frame(width: geometry.size.width, height: geometry.size.width)
                 VStack(spacing: 0) {
-                    let imageSize = size / 2
-                    Image(uiImage: musicPlayer.song?.artwork?.image(at: CGSize(width: imageSize, height: imageSize)) ?? UIImage(imageLiteralResourceName: "Song"))
-                        .renderingMode(.original)
-                        .resizable()
-                        .scaledToFit()
-                        .cornerRadius(StyleConstant.cornerRadius)
-                        .frame(width: imageSize, height: imageSize)
-                        .padding(.top, size / 8)
-                    VStack(spacing: StyleConstant.Padding.tiny) {
-                        TitleView(musicPlayer.song?.primaryText)
-                        SecondaryTextView(musicPlayer.song?.secondaryText)
-                    }
-                    .frame(height: size / 6)
-                    .padding(.horizontal, StyleConstant.Padding.large)
-
-                    Slider(value: $musicPlayer.currentSecond, in: 0 ... musicPlayer.durationSecond, step: 1, onEditingChanged: { changed in
-                        if musicPlayer.isPlaying, !seeking, changed {
-                            musicPlayer.stopProgress()
-                            seeking = changed
+                    VStack(spacing: 0) {
+                        let imageSize = isPad ? size * 0.5 : size * 0.6
+                        Spacer()
+                        Image(uiImage: musicPlayer.song?.artwork?.image(at: CGSize(width: imageSize, height: imageSize)) ?? UIImage(imageLiteralResourceName: "Song"))
+                            .renderingMode(.original)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(StyleConstant.cornerRadius)
+                            .frame(width: imageSize, height: imageSize)
+                            .padding(.top, size * 0.1)
+                        Spacer()
+                        VStack(spacing: StyleConstant.Padding.tiny) {
+                            TitleView(musicPlayer.song?.primaryText)
+                            SecondaryTextView(musicPlayer.song?.secondaryText)
                         }
-
-                        musicPlayer.seek(time: musicPlayer.currentSecond)
-
-                        if musicPlayer.isPlaying, seeking, !changed {
-                            musicPlayer.startProgress()
-                            seeking = changed
-                        }
-                    })
-                        .padding(.leading, 40)
-                        .padding(.trailing, 40)
+                        .padding(.horizontal, StyleConstant.Padding.large)
+                        Spacer()
+                    }.frame(height: height * 0.55)
+                    VStack(spacing: 0) {
+                        Slider(value: $musicPlayer.currentSecond, in: 0 ... musicPlayer.durationSecond, step: 1, onEditingChanged: { changed in
+                            if musicPlayer.isPlaying, !seeking, changed {
+                                musicPlayer.stopProgress()
+                                seeking = changed
+                            }
+                            musicPlayer.seek(time: musicPlayer.currentSecond)
+                            if musicPlayer.isPlaying, seeking, !changed {
+                                musicPlayer.startProgress()
+                                seeking = changed
+                            }
+                        })
+                        .padding(.horizontal, isPad ? 40 : 30)
                         .accentColor(Color(.label))
-
-                    HStack {
-                        SecondaryTextView("\(convertTime(time: musicPlayer.currentSecond))").frame(width: 50).padding(.leading, 40)
-                        Spacer()
-                        SecondaryTextView("\(convertTime(time: musicPlayer.durationSecond))").frame(width: 50).padding(.trailing, 40)
-                    }
-
-                    Spacer()
-
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            musicPlayer.doPrev()
-                        }) {
-                            Image("PrevButton").renderingMode(.original).resizable().frame(width: 88, height: 88)
+                        HStack {
+                            SecondaryTextView("\(convertTime(time: musicPlayer.currentSecond))").frame(width: 50).padding(.leading, 40)
+                            Spacer()
+                            SecondaryTextView("\(convertTime(time: musicPlayer.durationSecond))").frame(width: 50).padding(.trailing, 40)
                         }
-
                         Spacer()
-
-                        if musicPlayer.isPlaying {
+                        HStack {
+                            Spacer()
                             Button(action: {
-                                musicPlayer.doPause()
+                                musicPlayer.doPrev()
                             }) {
-                                Image("PauseButton").renderingMode(.original).resizable().frame(width: 100, height: 100)
+                                Image("PrevButton").renderingMode(.original).resizable().frame(width: 88, height: 88)
                             }
-                        } else {
+                            Spacer()
+                            if musicPlayer.isPlaying {
+                                Button(action: {
+                                    musicPlayer.doPause()
+                                }) {
+                                    Image("PauseButton").renderingMode(.original).resizable().frame(width: 100, height: 100)
+                                }
+                            } else {
+                                Button(action: {
+                                    musicPlayer.doPlay()
+                                }) {
+                                    Image("PlayButton").renderingMode(.original).resizable().frame(width: 100, height: 100)
+                                }
+                            }
+                            Spacer()
                             Button(action: {
-                                musicPlayer.doPlay()
+                                musicPlayer.doNext()
                             }) {
-                                Image("PlayButton").renderingMode(.original).resizable().frame(width: 100, height: 100)
+                                Image("NextButton").renderingMode(.original).resizable().frame(width: 88, height: 88)
                             }
+                            Spacer()
                         }
-
                         Spacer()
-
-                        Button(action: {
-                            musicPlayer.doNext()
-                        }) {
-                            Image("NextButton").renderingMode(.original).resizable().frame(width: 88, height: 88)
-                        }
-
-                        Spacer()
-                    }
-
-                    Spacer()
-
-                    HStack {
-                        Button(action: {
-                            musicPlayer.changeRepeat()
-                        }) {
-                            if musicPlayer.isRepeatOff {
-                                Image("RepeatButton").renderingMode(.original).resizable().frame(width: 50, height: 50).opacity(0.5)
-                            } else if musicPlayer.isRepeatAll {
-                                Image("RepeatButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                            } else if musicPlayer.isRepeatOne {
-                                Image("RepeatOneButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
+                        HStack {
+                            Button(action: {
+                                musicPlayer.changeRepeat()
+                            }) {
+                                if musicPlayer.isRepeatOff {
+                                    Image("RepeatButton").renderingMode(.original).resizable().frame(width: 50, height: 50).opacity(0.5)
+                                } else if musicPlayer.isRepeatAll {
+                                    Image("RepeatButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
+                                } else if musicPlayer.isRepeatOne {
+                                    Image("RepeatOneButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
+                                }
                             }
-                        }
-                        .frame(width: StyleConstant.button, height: StyleConstant.button)
-
-                        Spacer()
-
-                        Button(action: {
-                            musicPlayer.shuffle()
-                        }) {
-                            if musicPlayer.shuffleMode {
-                                Image("ShuffleButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                            } else {
-                                Image("ShuffleButton").renderingMode(.original).resizable().frame(width: 50, height: 50).opacity(0.5)
+                            .frame(width: StyleConstant.button, height: StyleConstant.button)
+                            Spacer()
+                            Button(action: {
+                                musicPlayer.shuffle()
+                            }) {
+                                if musicPlayer.shuffleMode {
+                                    Image("ShuffleButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
+                                } else {
+                                    Image("ShuffleButton").renderingMode(.original).resizable().frame(width: 50, height: 50).opacity(0.5)
+                                }
                             }
-                        }
-                        .frame(width: StyleConstant.button, height: StyleConstant.button)
-
-                        Spacer()
-
-                        Button(action: {
-                            musicPlayer.favoriteArtist()
-                        }) {
-                            if musicPlayer.isFavoriteArtist {
-                                Image("FavoriteArtistButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                            } else {
-                                Image("FavoriteArtistButton").renderingMode(.original).resizable().frame(width: 50, height: 50).opacity(0.5)
+                            .frame(width: StyleConstant.button, height: StyleConstant.button)
+                            Spacer()
+                            Button(action: {
+                                musicPlayer.favoriteArtist()
+                            }) {
+                                if musicPlayer.isFavoriteArtist {
+                                    Image("FavoriteArtistButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
+                                } else {
+                                    Image("FavoriteArtistButton").renderingMode(.original).resizable().frame(width: 50, height: 50).opacity(0.5)
+                                }
                             }
-                        }
-                        .frame(width: StyleConstant.button, height: StyleConstant.button)
-
-                        Spacer()
-
-                        Button(action: {
-                            musicPlayer.favoriteSong()
-                        }) {
-                            if musicPlayer.isFavoriteSong {
-                                Image("FavoriteSongButton").renderingMode(.original).resizable().frame(width: 40, height: 40)
-                            } else {
-                                Image("FavoriteSongButton").renderingMode(.original).resizable().frame(width: 40, height: 40).opacity(0.5)
+                            .frame(width: StyleConstant.button, height: StyleConstant.button)
+                            Spacer()
+                            Button(action: {
+                                musicPlayer.favoriteSong()
+                            }) {
+                                if musicPlayer.isFavoriteSong {
+                                    Image("FavoriteSongButton").renderingMode(.original).resizable().frame(width: 40, height: 40)
+                                } else {
+                                    Image("FavoriteSongButton").renderingMode(.original).resizable().frame(width: 40, height: 40).opacity(0.5)
+                                }
                             }
+                            .frame(width: StyleConstant.button, height: StyleConstant.button)
+                            Spacer()
+                            Button(action: {
+                                showingPopup.toggle()
+                            }) {
+                                Image("PlaylistAddButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
+                            }
+                            .frame(width: StyleConstant.button, height: StyleConstant.button)
                         }
-                        .frame(width: StyleConstant.button, height: StyleConstant.button)
-
+                        .padding(.horizontal, isPad ? 50 : 30)
                         Spacer()
-
-                        Button(action: {
-                            showingPopup.toggle()
-                        }) {
-                            Image("PlaylistAddButton").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                        }
-                        .frame(width: StyleConstant.button, height: StyleConstant.button)
-                    }
-                    .padding(.horizontal, 50)
-
-                    Spacer()
+                    }.frame(height: height * 0.45)
                 }
                 if showingPopup {
                     PopupView(showingPopup: $showingPopup) {
